206066b1c64a09557a1e1cd64c8b71f22a5e8990
JoeCoughlin <Joseph_Coughlin@cable.comcast.com>
2021-07-09 10:23:46 -0400

    Tests working

--- src/main/java/com/company/PositionType.java
+++ src/main/java/com/company/PositionType.java
@@ -1,30 +1,60 @@
 package com.company;
+import java.util.Objects;
 
-public class PositionType {
-    private static int nextId = 1;
-
-    private int id;
-    private String value;
-
+public class PositionType extends JobField{
     public PositionType() {
-        id = nextId;
-        nextId++;
     }
 
     public PositionType(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
 
-    // TODO: Add a custom toString() method that returns the data stored in 'value'.
-
-    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when their id fields match.
-
-    // Getters and Setters:
-
-    public int getId() { return this.id; };
-
-    public String getValue() { return this.value; }
-
-    public void setValue(String value) { this.value = value; }
 }
+
+//public class PositionType {
+//    private static int nextId = 1;
+//
+//    private int id;
+//    private String value;
+//
+//    public PositionType() {
+//        id = nextId;
+//        nextId++;
+//    }
+//
+//    public PositionType(String value) {
+//        this();
+//        this.value = value;
+//    }
+//
+//    // TODO: Add a custom toString() method that returns the data stored in 'value'.
+//
+//    @Override
+//    public String toString() {
+//        return value;
+//    }
+//
+//
+//    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when their id fields match.
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        PositionType that = (PositionType) o;
+//        return getId() == that.getId();
+//    }
+//
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id);
+//    }
+//    // Getters and Setters:
+//
+//    public int getId() { return this.id; };
+//
+//    public String getValue() { return this.value; }
+//
+//    public void setValue(String value) { this.value = value; }
+//}
--- src/test/java/com/company/incomplete/JobTests.java
+++ src/test/java/com/company/incomplete/JobTests.java
@@ -1,38 +1,106 @@
 package com.company.incomplete;
 
+import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import com.company.*;
 
 public class JobTests {
-    // you can rename the tests, or delete them and rewrite them from scratch, but I went ahead and provided you the names of tests I created -- I followed the writeup pretty honestly
-    @Test
-    public void testSettingJobId() {
+    static Job test1, test2, test3, test4, test5;
+
+    @BeforeAll
+    static void createJobObject() {
+        test1 = new Job();
+        test2 = new Job();
+        test3 = new Job("Mild-Mannered Reporter",
+                new Employer("Daily Planet"),
+                new Location("Metropolis"),
+                new PositionType("Political Reporter"),
+                new CoreCompetency("Flight"));
+        test4 = new Job("Mild-Mannered Reporter",
+                new Employer("Daily Planet"),
+                new Location("Metropolis"),
+                new PositionType(""),
+                new CoreCompetency("Flight"));
+        test5 = new Job("",
+                new Employer(""),
+                new Location(""),
+                new PositionType(""),
+                new CoreCompetency(""));
 
     }
 
     @Test
-    public void testJobConstructorSetsAllFields() {
+    void testSettingJobId () {
+        Integer id1 = test1.getId();
+        Integer id2 = test2.getId();
+        assertEquals(1, id2-id1);
+    }
+
+    @Test
+    void testJobConstructorSetAllFields () {
+        assertTrue(test3.getName() instanceof String);
+        assertTrue(test3.getEmployer() instanceof Employer);
+        assertTrue(test3.getLocation() instanceof Location);
+        assertTrue(test3.getCoreCompetency() instanceof CoreCompetency);
+        assertTrue(test3.getPositionType() instanceof PositionType);
+    }
+
+    @Test
+    void testJobsForEquality () {
+        assertNotEquals(test3, test4);
+    }
+
+    @Test
+    void testToStringEmpty () {
+        assertTrue(test5.toString().startsWith("\n"));
+    }
+
+    @Test
+    void testToStringEmpty1 () {
+        assertTrue(test5.toString().endsWith("\n"));
+    }
+
+    @Test
+    public void testToStringStartsAndEndsWithNewLine() {
+        char firstChar = test4.toString().charAt(0);
+        char lastChar = test4.toString().charAt(test4.toString().length() - 1);
+        assertTrue(firstChar == lastChar);
+    }
+
+    @Test
+    void  testToStringContainsBlankLines() {
+        assertTrue(test1.toString().startsWith("\n"));
+        assertTrue(test1.toString().endsWith("\n"));
 
     }
 
     @Test
-    public void testJobsForEquality() {
-
-    }
-
-    // I found the instructions around the three toString() tests a little confusing, so I created three tests following the three bullet points:
-    // you can follow TDD if you want, or you can write the implementation first and then get the tests to pass, either is a fine approach
-    @Test
-    public void testToStringContainsBlankLines() {
-
-    }
-
-    @Test
-    public void testToStringHasLabelsForEachField() {
-
+    public void testToStringContainsCorrectLabels() {
+        String output = String.format("\nID: %d\n" +
+                        "Name: %s\n" +
+                        "Employer: %s\n" +
+                        "Location: %s\n" +
+                        "Position Type: %s\n" +
+                        "Core Competency: %s\n", test3.getId(), test3.getName(), test3.getEmployer(), test3.getLocation(),
+                test3.getPositionType(), test3.getCoreCompetency());
+        String actual = test3.toString();
+        assertEquals(output, test3.toString());
     }
 
     @Test
-    public void testToStringDataNotAvailable() {
-
+    public void testForDataNotAvailable() {
+        String expected = String.format("\nID: %d\n" +
+                        "Name: %s\n" +
+                        "Employer: %s\n" +
+                        "Location: %s\n" +
+                        "Position Type: %s\n" +
+                        "Core Competency: %s\n",test4.getId(), test4.getName(), test4.getEmployer(), test4.getLocation(),
+                test4.getPositionType(), test4.getCoreCompetency());
+        String actual = test4.toString();
+        assertEquals(expected, actual);
     }
+
+
+
 }
